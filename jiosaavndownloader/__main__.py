c0cb5375d595c728c34864be66e41ca8f2b4add4
Vijaymahantesh Sattigeri <vijaymahantesh016@gmail.com>
2022-04-11 15:33:16 +0530

    Feauture: Added -o option to specify media output directory

--- jiosaavndownloader/__main__.py
+++ jiosaavndownloader/__main__.py
@@ -1,6 +1,19 @@
 
 import downloader
 import argparse
+import os
+
+
+def createOutDir(a_dir_path):
+    try:
+        os.makedirs(a_dir_path, exist_ok=True)
+
+    except OSError as exc:
+        raise
+
+    if not os.access(a_dir_path, os.W_OK):
+        raise IOError("No write permission for the destination directory!")
+
 
 class ReadFromFile(argparse.Action):
     def __call__ (self, parser, namespace, values, option_string = None):
@@ -10,16 +23,20 @@ class ReadFromFile(argparse.Action):
 
 
 if __name__ == '__main__':
-    arg_parser = argparse.ArgumentParser(description = 'Downloads music from JioSaavn')
+    arg_parser = argparse.ArgumentParser(description = 'Downloads music from JioSaavn', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     arg_parser.add_argument('-u', '--url', type=str, metavar='', action="append", help='The URL of the song, album or playlist')
     arg_parser.add_argument('-f', '--file', type=open, action=ReadFromFile, metavar='', help='The file containing options')
+    arg_parser.add_argument('-o', '--output_dir', type=str, metavar='', nargs='?', default="MusicLibrary", const="MusicLibrary", help='The destination directory to store the downloaded media')
 
     args = arg_parser.parse_args()
+
+    # Create output directory 
+    createOutDir(args.output_dir)
     
     for each_url in args.url:
         each_url = each_url.rstrip()
         #print(each_url)
         song_data, music_type = downloader.get_media_data(each_url)
         #print(song_data)
-        d_obj = downloader.Downloader(song_data, music_type)
+        d_obj = downloader.Downloader(song_data, music_type, args.output_dir)
         d_obj.downloadMusic()
